4cad8d5b4551ebcfd045e4ba95f03910b02b7b14
Nguyen Quoc Khanh <20521452@gm.uit.edu.vn>
2023-01-04 18:30:16 +0700

    updatee

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  serverRuntimeConfig: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
+
+module.exports = nextConfig;
